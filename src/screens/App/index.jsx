22c6dc707fd25cf6cc19c28a1dbd6c5bbd9968d6
kiotaku <kkutalol@gmail.com>
2017-10-16 10:14:11 +0900

    AlertContainerの追加

--- src/screens/App/index.jsx
+++ src/screens/App/index.jsx
@@ -1,5 +1,7 @@
-import { h } from 'preact'
+import { h, Component } from 'preact'
 import { BrowserRouter, Link, Route, Switch, withRouter } from 'react-router-dom'
+import AlertContainer from 'react-alert'
+
 // import Baccarat from './screens/Baccarat'
 // import Blackjack from './screens/Blackjack'
 // import Poker from './screens/Poker'
@@ -9,17 +11,30 @@ import Wrapper from './components/Wrapper'
 
 const Tmp = () => (<Link to='/resister_user'>resister user</Link>)
 
-export default () => (
-  <BrowserRouter>
-    <div className='app'>
-      <Wrapper>
-        <Route exact path='/' component={ Tmp }/>
-        {/* { Baccarat } */}
-        {/* { Blackjack } */}
-        {/* { Poker } */}
-        <ResisterUser />
-        {/* { Settings } */}
-      </Wrapper>
-    </div>
-  </BrowserRouter>
-)
+export default class App extends Component {
+  var alertOptions = {
+    offset: 14,
+    position: 'bottom right',
+    theme: 'light',
+    time: 5000,
+    transition: 'fade'
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <AlertContainer ref={a => wimdow.alert_msg = a}, {...this.alertOptions}/>
+        <div className='app'>
+          <Wrapper>
+            <Route exact path='/' component={ Tmp }/>
+            {/* { Baccarat } */}
+            {/* { Blackjack } */}
+            {/* { Poker } */}
+            <ResisterUser />
+            {/* { Settings } */}
+          </Wrapper>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
